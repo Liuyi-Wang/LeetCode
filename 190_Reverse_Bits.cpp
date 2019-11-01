c049cff2b397591a7e7867d860cbd161be06933d
Liuyi Wang <wangliuy@umich.edu>
2019-11-01 16:43:57 -0400

    E

---
+++ 190_Reverse_Bits.cpp
@@ -0,0 +1,17 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        for (int i = 0; i < 32; ++i) {
+            result = (result<<1) | ((n>>i)&1); 
+        }
+        return result;
+    }
+};
