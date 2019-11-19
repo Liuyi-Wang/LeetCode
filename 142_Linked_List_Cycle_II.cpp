455e03188fdc734bb41f2d4f62ed7311b0455693
Liuyi Wang <wangliuy@umich.edu>
2019-11-18 22:05:05 -0500

    M

---
+++ 142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,44 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (NULL == head) {
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (NULL == fast->next || NULL == fast->next->next) {
+            return NULL;
+        }
+        fast = head;
+        while (true) {
+            if (fast == slow) {
+                break;
+            }
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return fast;
+    }
+};
