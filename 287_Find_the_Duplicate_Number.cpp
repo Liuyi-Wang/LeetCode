cab60458279688fc54b8250c7672c4a3545581bd
Liuyi Wang <wangliuy@umich.edu>
2019-11-18 21:53:06 -0500

    M

---
+++ 287_Find_the_Duplicate_Number.cpp
@@ -0,0 +1,29 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+        }
+        int t = 0;
+        while (true) {
+            slow = nums[slow];
+            t = nums[t];
+            if (slow == t) {
+                break;
+            }
+        }
+        return slow;
+    }
+};
