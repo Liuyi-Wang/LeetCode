72ac28814bff1c20eaedcfd99c0d974abf07e96b
Liuyi Wang <wangliuy@umich.edu>
2019-11-30 10:26:56 -0500

    E

---
+++ 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -0,0 +1,33 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val == q->val) {
+            return p;
+        }
+        int l = min(p->val, q->val);
+        int r = max(p->val, q->val);
+        if (l <= root->val && r >= root->val) {
+            return root;
+        } else if (l < root->val && r < root->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+    }
+};
