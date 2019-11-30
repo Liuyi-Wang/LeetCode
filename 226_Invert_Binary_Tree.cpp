a65299c7367f778b1af53e238fa37b7bd1f66053
Liuyi Wang <wangliuy@umich.edu>
2019-11-29 22:07:12 -0500

    E

---
+++ 226_Invert_Binary_Tree.cpp
@@ -0,0 +1,28 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (NULL == root) {
+            return NULL;
+        }
+        TreeNode *node = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(node);
+        return root;
+    }
+};
