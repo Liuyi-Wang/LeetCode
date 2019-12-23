63d865642eb4a32b35c14a68e7fae920a687b659
Liuyi Wang <wangliuy@umich.edu>
2019-12-22 23:02:11 -0500

    E

---
+++ 339_Nested_List_Weight_Sum.cpp
@@ -0,0 +1,54 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    int solve(const vector<NestedInteger>& nestedList, const int level) {
+        int result = 0;
+        for (int i = 0; i < nestedList.size(); ++i) {
+            if (nestedList[i].isInteger()) {
+                result += nestedList[i].getInteger()*level;
+            } else {
+                result += solve(nestedList[i].getList(), level+1);
+            }
+        }
+        return result;
+    }
+    
+    int depthSum(vector<NestedInteger>& nestedList) {
+        return solve(nestedList, 1);
+    }
+};
