36435e84cc469a0f9b128153c86e38c5b6d288d0
Liuyi Wang <wangliuy@umich.edu>
2019-12-25 11:02:19 -0500

    E

---
+++ 617_Merge_Two_Binary_Trees.cpp
@@ -0,0 +1,34 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (NULL == t1 && NULL == t2) {
+            return NULL;
+        }
+        if (NULL == t1) {
+            return t2;
+        }
+        if (NULL == t2) {
+            return t1;
+        }
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
