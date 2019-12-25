f1ab377e271c927a6bcfccb9479c2977c9539a32
Liuyi Wang <wangliuy@umich.edu>
2019-12-25 11:22:49 -0500

    E

---
+++ 628_Maximum_Product_of_Three_Numbers.cpp
@@ -0,0 +1,33 @@
+static int __ = []() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
+}();
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+        int min1 = INT_MAX, min2 = INT_MAX;
+        for (auto n:nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        return max(max1*max2*max3, max1*min1*min2);
+    }
+};
