8522c25467da220ed85a1f0e59cc9dba32f901ce
Liuyi Wang <wangliuy@umich.edu>
2020-04-14 20:58:23 -0400

    M

---
+++ 650_2Keys_Keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int result = 0;
+        for (int i = 2; i <= n; ++i) {
+            /// find prime
+            while (n%i == 0) {
+                result += i;
+                n /= i;
+            }
+        }
+        return result;
+    }
+};
