1c6d967dc87eb101ccbe0b945aac9183dd7510f2
Liuyi Wang <wangliuy@umich.edu>
2021-01-11 21:46:25 -0500

    M

--- 116_Populating_Next_Right_Pointers_in_Each_Node.cpp
+++ 116_Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -1,10 +1,3 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /*
 // Definition for a Node.
 class Node {
@@ -14,32 +7,27 @@ public:
     Node* right;
     Node* next;
 
-    Node() {}
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
 
-    Node(int _val, Node* _left, Node* _right, Node* _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
 };
 */
+
 class Solution {
 public:
-    void solve(Node *root) {
+    Node* connect(Node* root) {
         if (NULL == root || NULL == root->left) {
-            return;
+            return root;
         }
         root->left->next = root->right;
-        if (root->next) {
+        if (NULL != root->next) {
             root->right->next = root->next->left;
         }
-        solve(root->left);
-        solve(root->right);
-    }
-    
-    Node* connect(Node* root) {
-        solve(root);
+        connect(root->left);
+        connect(root->right);
         return root;
     }
 };
