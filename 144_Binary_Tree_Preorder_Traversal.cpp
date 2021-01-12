5d522b1c99b6bade4a046b4092f4f504e86cdede
Liuyi Wang <wangliuy@umich.edu>
2021-01-12 09:34:03 -0500

    M

--- 144_Binary_Tree_Preorder_Traversal.cpp
+++ 144_Binary_Tree_Preorder_Traversal.cpp
@@ -1,38 +1,29 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> result;
-        if (NULL == root) {
-            return result;
-        }
+        TreeNode* it = root;
         stack<TreeNode*> s;
-        s.push(root);
-        while (!s.empty()) {
-            root = s.top();
+        while (it || !s.empty()) {
+            while (it) {
+                result.push_back(it->val);
+                s.push(it);
+                it = it->left;
+            }
+            it = s.top();
             s.pop();
-            result.push_back(root->val);
-            if (root->right) {
-                s.push(root->right);
-            }
-            if (root->left) {
-                s.push(root->left);
-            }
+            it = it->right;
         }
         return result;
     }
