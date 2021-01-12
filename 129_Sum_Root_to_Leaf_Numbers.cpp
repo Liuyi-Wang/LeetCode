adb3c641a0e4104eac9e1d3881acc493e96ca02b
Liuyi Wang <wangliuy@umich.edu>
2021-01-12 09:17:38 -0500

    M

--- 129_Sum_Root_to_Leaf_Numbers.cpp
+++ 129_Sum_Root_to_Leaf_Numbers.cpp
@@ -1,43 +1,38 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int sum) {
-        if (NULL == root->left && NULL == root->right) {
-            d_result += sum*10+root->val;
-            return;
-        }
-        if (root->left) {
-            solve(root->left, sum*10+root->val);
-        }
-        if (root->right) {
-            solve(root->right, sum*10+root->val);
-        }
-    }
-    
     int sumNumbers(TreeNode* root) {
         if (NULL == root) {
             return 0;
         }
-        d_result = 0;
-        solve(root, 0);
-        return d_result;
+        int result = 0;
+        int number = 0;
+        recursion(root, result, number);
+        return result;
     }
     
-private:
-    int d_result;
+    void recursion(TreeNode* root, int& result, int& number) {
+        if (NULL == root->left && NULL == root->right) {
+            result += number*10+root->val;
+            return;
+        }
+        number = number*10+root->val;
+        if (root->left) {
+            recursion(root->left, result, number);
+        }
+        if (root->right) {
+            recursion(root->right, result, number);
+        }
+        number = (number-root->val)/10;
+    }
 };
