232d9527c988df5844f60832e3d09ef924cf2451
Liuyi Wang <wangliuy@umich.edu>
2021-01-11 21:37:02 -0500

    M

--- 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
+++ 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -1,39 +1,33 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* solve(const vector<int> &inorder, int ib, int ie,
-                    const vector<int> &postorder, int pb, int pe) {
-        if (pb > pe) {
-            return NULL;
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int, int> imap;
+        for (int i = 0; i < inorder.size(); ++i) {
+            imap[inorder[i]] = i;
         }
-        TreeNode *node = new TreeNode(postorder[pe]);
-        int i = ib;
-        for (; i <= ie; ++i) {
-            if (inorder[i] == postorder[pe]) {
-                break;
-            }
-        }
-        node->left = solve(inorder, ib, i-1, postorder, pb, pb+i-1-ib);
-        node->right = solve(inorder, i+1, ie, postorder, pb+i-ib, pe-1);
-        return node;
+        return recursion(inorder, 0, inorder.size()-1, postorder, 0, postorder.size()-1, imap);
     }
     
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return solve(inorder, 0, inorder.size()-1, postorder, 0, postorder.size()-1);
+    TreeNode* recursion(const vector<int>& iorder, int ib, int ie,
+                        const vector<int>& porder, int pb, int pe,
+                        const unordered_map<int, int>& imap) {
+        if (ib > ie) {
+            return NULL;
+        }
+        int index = imap.find(porder[pe])->second;
+        TreeNode* l = recursion(iorder, ib, index-1, porder, pb, pb+index-1-ib, imap);
+        TreeNode* r = recursion(iorder, index+1, ie, porder, pb+index-ib, pe-1, imap);
+        return new TreeNode(porder[pe], l, r);
     }
 };
