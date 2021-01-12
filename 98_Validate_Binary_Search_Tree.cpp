cf4bf4197dd63d1e3165a753adfd1f9c006be3d3
Liuyi Wang <wangliuy@umich.edu>
2021-01-11 21:32:04 -0500

    M

--- 98_Validate_Binary_Search_Tree.cpp
+++ 98_Validate_Binary_Search_Tree.cpp
@@ -4,36 +4,44 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isValid(TreeNode* root, long long& maxV, long long& minV) {
+    bool isValidBST(TreeNode* root) {
         if (NULL == root) {
             return true;
         }
-        long long lMax = LLONG_MIN, lMin = LLONG_MAX;
-        if (!isValid(root->left, lMax, lMin)) {
-            return false;
-        } 
-        long long rMax = LLONG_MIN, rMin = LLONG_MAX;
-        if (!isValid(root->right, rMax, rMin)) {
-            return false;
-        } 
-        if (lMax >= root->val) {
-            return false;
-        }
-        if (rMin <= root->val) {
-            return false;
-        }
-        maxV = max(static_cast<long long>(root->val), rMax);
-        minV = min(static_cast<long long>(root->val), lMin);
-        return true;
+        int min_val = root->val, max_val = root->val;
+        return recursion(root, min_val, max_val);
     }
     
-    bool isValidBST(TreeNode* root) {
-        long long max = LLONG_MIN, min = LLONG_MAX;
-        return isValid(root, max, min);
+    bool recursion(TreeNode* root, int& min_val, int& max_val) {
+        int lmin = root->val;
+        int lmax = root->val;
+        int rmin = root->val;
+        int rmax = root->val;
+        if (root->left) {
+            if (!recursion(root->left, lmin, lmax)) {
+                return false;
+            }
+            if (lmax >= root->val) {
+                return false;
+            }
+        }
+        if (root->right) {
+            if (!recursion(root->right, rmin, rmax)) {
+                return false;
+            }
+            if (rmin <= root->val) {
+                return false;
+            }
+        }
+        min_val = lmin;
+        max_val = rmax;
+        return true;
     }
 };
