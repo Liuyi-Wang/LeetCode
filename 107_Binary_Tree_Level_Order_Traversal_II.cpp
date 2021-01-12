86ab03f97c15850430796cf1311016ea4740b37a
Liuyi Wang <wangliuy@umich.edu>
2021-01-11 21:38:05 -0500

    E

--- 107_Binary_Tree_Level_Order_Traversal_II.cpp
+++ 107_Binary_Tree_Level_Order_Traversal_II.cpp
@@ -1,48 +1,39 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        queue<TreeNode*> q;
-        if (root) {
-            q.push(root);
-        }
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
         vector<vector<int>> result;
+        if (NULL == root) {
+            return result;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
         while (!q.empty()) {
-            vector<int> level(q.size(), 0);
-            for (int i = 0; i < level.size(); ++i) {
-                root = q.front();
+            vector<int> v;
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
                 q.pop();
-                level[i] = root->val;
-                if (root->left) {
-                    q.push(root->left);
+                v.push_back(node->val);
+                if (node->left) {
+                    q.push(node->left);
                 }
-                if (root->right) {
-                    q.push(root->right);
+                if (node->right) {
+                    q.push(node->right);
                 }
             }
-            result.push_back(level);
+            result.insert(result.begin(), v);
         }
-        return result;
-    }
-    
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>> result = levelOrder(root);
-        reverse(result.begin(), result.end());
         return result;
     }
 };
