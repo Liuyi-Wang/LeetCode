686d7b459c515e2efafd8d6e36e386b4268f4c31
Liuyi Wang <wangliuy@umich.edu>
2021-01-11 21:32:50 -0500

    e

--- 100_Same_Tree.cpp
+++ 100_Same_Tree.cpp
@@ -1,17 +1,12 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -20,15 +15,15 @@ public:
         if (NULL == p && NULL == q) {
             return true;
         }
-        if (NULL == p || NULL == q) {
+        if (NULL == p && q) {
             return false;
-        } 
+        }
+        if (p && NULL == q) {
+            return false;
+        }
         if (p->val != q->val) {
             return false;
         }
-        if (isSameTree(p->left, q->left) && isSameTree(p->right, q->right)) {
-            return true;
-        }
-        return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
