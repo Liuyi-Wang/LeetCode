229011fe03910717005e60bda73f02c47989d07c
Liuyi Wang <wangliuy@umich.edu>
2021-01-11 21:43:15 -0500

    M

--- 113_Path_Sum_II.cpp
+++ 113_Path_Sum_II.cpp
@@ -4,39 +4,35 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode *root, const int &sum, int s, vector<int> &v) {
-        v.push_back(root->val);
-        s += root->val;
-        if (NULL == root->left && NULL == root->right) {
-            if (s == sum) {
-                d_result.push_back(v);
-            }
-            v.pop_back();
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> result;
+        vector<int> v;
+        recursion(root, sum, result, v);
+        return result;
+    }
+    
+    void recursion(TreeNode* root, int sum, vector<vector<int>>& result, vector<int>& v) {
+        if (NULL == root) {
             return;
         }
-        if (root->left) {
-            solve(root->left, sum, s, v);
+        if (NULL == root->left && NULL == root->right) {
+            if (root->val == sum) {
+                v.push_back(root->val);
+                result.push_back(v);
+                v.pop_back();
+            }
+            return;
         }
-        if (root->right) {
-            solve(root->right, sum, s, v);
-        }
+        v.push_back(root->val);
+        recursion(root->left, sum-root->val, result, v);
+        recursion(root->right, sum-root->val, result, v);
         v.pop_back();
     }
-    
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        if (NULL == root) {
-            return d_result;
-        }
-        vector<int> v;
-        solve(root, sum, 0, v);
-        return d_result;
-    }
-    
-private:
-    vector<vector<int>> d_result;
 };
