5ec6bc9befb39d7cc46b6fe5e649164492771dc9
Liuyi Wang <wangliuy@umich.edu>
2021-01-11 21:39:15 -0500

    E

--- 110_Balanced_Binary_Tree.cpp
+++ 110_Balanced_Binary_Tree.cpp
@@ -1,45 +1,38 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool solve(TreeNode* root, int &h) {
+    bool isBalanced(TreeNode* root) {
+        int height = 0;
+        return recursion(root, height);
+    }
+    
+    bool recursion(TreeNode* root, int &height) {
         if (NULL == root) {
-            h = 0;
+            height = 0;
             return true;
         }
-        int lh;
-        bool l = solve(root->left, lh);
-        if (!l) {
+        int lh = 0;
+        if (!recursion(root->left, lh)) {
             return false;
         }
-        int rh;
-        bool r = solve(root->right, rh);
-        if (!r) {
+        int rh = 0;
+        if (!recursion(root->right, rh)) {
             return false;
         }
         if (abs(lh-rh) > 1) {
             return false;
         }
-        h = max(lh, rh)+1;
+        height = max(lh, rh) + 1;
         return true;
-    }
-    
-    bool isBalanced(TreeNode* root) {
-        int h;
-        return solve(root, h);
     }
 };
