1491c04716eb1c216dc352b36a864aa9782a615f
Liuyi Wang <wangliuy@umich.edu>
2021-01-13 19:08:30 -0500

    M

--- 230_kth_Smallest_Element_in_a_BST.cpp
+++ 230_kth_Smallest_Element_in_a_BST.cpp
@@ -4,37 +4,43 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        stack<TreeNode*> s;
-        s.push(root);
         int count = 0;
-        while (!s.empty()) {
-            root = s.top();
-            s.pop();
-            if (NULL == root->left && NULL == root->right) {
-                ++count;
-                if (count == k) {
-                    return root->val;
-                }
-                continue;
-            }
-            TreeNode *l = root->left;
-            TreeNode *r = root->right;
-            root->left = NULL;
-            root->right = NULL;
-            if (r) {
-                s.push(r);
-            }
-            s.push(root);
-            if (l) {
-                s.push(l);
-            }
+        bool find = false;
+        return recursion(root, k, count, find);
+    }
+    
+    int recursion(TreeNode* root, const int k, int& count, bool& find) {
+        if (NULL == root) {
+            count = 0;
+            return 0;
         }
+        int lc = 0;
+        bool lf = false;
+        int l = recursion(root->left, k, lc, lf);
+        if (lf) {
+            find = true;
+            return l;
+        }
+        if (lc+1 == k) {
+            find = true;
+            return root->val;
+        }
+        int rc = 0;
+        bool rf = false;
+        int r = recursion(root->right, k-1-lc, rc, rf);
+        if (rf) {
+            find = true;
+            return r;
+        }
+        count = lc + 1 + rc;
         return 0;
     }
 };
