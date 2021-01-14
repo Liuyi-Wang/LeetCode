8a2af4e6b84e8cd1eadc9145cf233a1377ec4c19
Liuyi Wang <wangliuy@umich.edu>
2021-01-13 19:20:34 -0500

    E

--- 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
+++ 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -1,10 +1,3 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -14,20 +7,34 @@ static int __ = []() {
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
+    /*   
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (p->val == q->val) {
-            return p;
-        }
-        int l = min(p->val, q->val);
-        int r = max(p->val, q->val);
-        if (l <= root->val && r >= root->val) {
+        if (root->val == p->val || root->val == q->val) {
             return root;
-        } else if (l < root->val && r < root->val) {
-            return lowestCommonAncestor(root->left, p, q);
-        } else {
+        }
+        if ((root->val > p->val && root->val < q->val) || (root->val < p->val && root->val > q->val)) {
+            return root;
+        }
+        if (root->val < p->val) {
             return lowestCommonAncestor(root->right, p, q);
+        }
+        return lowestCommonAncestor(root->left, p, q);
+    }
+    */
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (true) {
+            if (root->val < p->val && root->val < q->val) {
+                root = root->right;
+                continue;
+            }
+            if (root->val > p->val && root->val > q->val) {
+                root = root->left;
+                continue;
+            }
+            return root;
         }
     }
 };
