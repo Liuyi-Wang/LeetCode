54b67925bcd686b3d067651657003c70336fd419
Liuyi Wang <wangliuy@umich.edu>
2021-07-08 07:38:23 -0400

    M

--- 186_Reverse_Words_in_a_String_II.cpp
+++ 186_Reverse_Words_in_a_String_II.cpp
@@ -1,35 +1,18 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 class Solution {
 public:
     void reverseWords(vector<char>& s) {
         reverse(s.begin(), s.end());
-        int l = 0, r = 0;
-        int index = 0;
-        while (true) {
-            while (l < s.size() && ' ' == s[l]) {
-                ++l;
+		int i = 0;
+        int l = i;
+        while (i < s.size()) {
+            if (s[i] != ' ') {
+                ++i;
+                continue;
             }
-            if (l == s.size()) {
-                break;
-            }
-            r = l;
-            while (r < s.size() && ' ' != s[r]) {
-                ++r;
-            }
-            reverse(s.begin()+l, s.begin()+r);
-            for (int i = l; i < r; ++i) {
-                s[index++] = s[i];
-            }
-            if (index < s.size()) {
-                s[index++] = ' ';
-            }
-            l = r;
+            reverse(s.begin()+l, s.begin()+i);
+            ++i;
+            l = i;
         }
+        reverse(s.begin()+l, s.begin()+i);
     }
 };
