b9a0230dfd634fe16b8fdf28c1dd18c7efc408e8
Liuyi Wang <wangliuy@umich.edu>
2021-10-04 00:08:16 -0400

    E

--- src/463_Island_Perimeter.cpp
+++ src/463_Island_Perimeter.cpp
@@ -1,31 +1,24 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
         int result = 0;
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[i].size(); ++j) {
-                if (0 == grid[i][j]) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) {
                     continue;
                 }
-                result += 4;
-                if (i-1 >= 0 && 1 == grid[i-1][j]) {
-                    --result;
+                if (i-1 < 0 || grid[i-1][j] == 0) {
+                    ++result;
                 }
-                if (j-1 >= 0 && 1 == grid[i][j-1]) {
-                    --result;
+                if (i+1 >= m || grid[i+1][j] == 0) {
+                    ++result;
                 }
-                if (i+1 < grid.size() && 1 == grid[i+1][j]) {
-                    --result;
+                if (j-1 < 0 || grid[i][j-1] == 0) {
+                    ++result;
                 }
-                if (j+1 < grid[i].size() && 1 == grid[i][j+1]) {
-                    --result;
+                if (j+1 >= n || grid[i][j+1] == 0) {
+                    ++result;
                 }
             }
         }
