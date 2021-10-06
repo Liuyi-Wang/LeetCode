886021c9b40e8965d23bebf1e8e8eb667907311e
Liuyi Wang <wangliuy@umich.edu>
2021-10-05 22:07:11 -0400

    M

--- src/442_Find_All_Duplicates_in_an_Array.cpp
+++ src/442_Find_All_Duplicates_in_an_Array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
         vector<int> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            int j = abs(nums[i])-1;
-            if (nums[j] < 0) {
+        for (int i = 0; i < n; ++i) {
+            if (nums[abs(nums[i])-1] < 0) {
                 result.push_back(abs(nums[i]));
             } else {
-                nums[j] = -nums[j];
+                nums[abs(nums[i])-1] = -nums[abs(nums[i])-1];
             }
         }
         return result;
