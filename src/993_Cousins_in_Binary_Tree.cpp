58b50adf11b15445aba254c13245e69b03012170
Liuyi Wang <wangliuy@umich.edu>
2021-10-17 20:22:16 -0400

    E

--- src/993_Cousins_in_Binary_Tree.cpp
+++ src/993_Cousins_in_Binary_Tree.cpp
@@ -4,56 +4,50 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* root, const int& x, const int& y, int depth, int parent) {
-        if (NULL == root) {
-            return;
+    bool isCousins(TreeNode* root, int x, int y) {
+        d_dx = -1;
+        d_dy = -1;
+        if (!sol(root, x, y, 0)) {
+            return false;
         }
-        if (x == root->val) {
-            d_x_p = parent;
-            d_x_d = depth+1;
-        }
-        if (y == root->val) {
-            d_y_p = parent;
-            d_y_d = depth+1;
-        }
-        if (-1 != d_x_d && -1 != d_y_d) {
-            return;
-        }
-        solve(root->left, x, y, depth+1, root->val);
-        if (-1 != d_x_d && -1 != d_y_d) {
-            return;
-        }
-        solve(root->right, x, y, depth+1, root->val);
+        return d_dx == d_dy;
     }
     
-    bool isCousins(TreeNode* root, int x, int y) {
-        if (x < 1 || x > 100 || y < 1 || y > 100) {
-            return false;
-        }
-        d_x_p = -1;
-        d_y_p = -1;
-        d_x_d = -1;
-        d_y_d = -1;
-        solve(root->left, x, y, 0, root->val); 
-        if (-1 == d_x_d || -1 == d_y_d) {
-            solve(root->right, x, y, 0, root->val); 
-        }
-        if (-1 == d_x_d || -1 == d_y_d) {
-            return false;
-        }
-        if (d_x_d == d_y_d && d_x_p != d_y_p) {
+    bool sol(TreeNode* root, int x, int y, int d) {
+        if (root == NULL) {
             return true;
         }
-        return false;
+        if (root->left && root->right) {
+            if (root->left->val == x && root->right->val == y) {
+                return false;
+            } else if (root->left->val == y && root->right->val == x) {
+                return false;
+            }
+        }
+        if (root->val == x) {
+            d_dx = d;
+        } else if (root->val == y) {
+            d_dy = d;
+        }
+        if (d_dx == -1 || d_dy == -1) {
+            if (!sol(root->left, x, y, d+1)) {
+                return false;
+            }
+            if (!sol(root->right, x, y, d+1)) {
+                return false;
+            }
+        }
+        return true;
     }
+    
 private:
-    int d_x_p;
-    int d_y_p;
-    int d_x_d;
-    int d_y_d;
+    int d_dx;
+    int d_dy;
 };
