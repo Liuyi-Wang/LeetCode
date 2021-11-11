6e3dc1ac4971bd8927fe4f5ba763c1bcfae6d8aa
Liuyi Wang <wangliuy@umich.edu>
2021-11-10 20:41:07 -0500

    E

--- src/1413_Minimum_Value_to_Get_Positive_Step_by_Step_Sum.cpp
+++ src/1413_Minimum_Value_to_Get_Positive_Step_by_Step_Sum.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int prev = 1-nums.back();
-        for (int i = static_cast<int>(nums.size())-2; i >= 0; --i) {
-            int t = max(1, prev);
-            prev = t-nums[i];
+        int num = INT_MAX;
+        int sum = 0;
+        for (auto n:nums) {
+            sum += n;
+            num = min(num, sum);
         }
-        return max(prev, 1);
+        return max(1-num, 1);
     }
 };
