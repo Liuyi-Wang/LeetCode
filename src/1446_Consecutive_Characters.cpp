9189c191a0407b95a6b018da038babbe74d4335b
Liuyi Wang <wangliuy@umich.edu>
2021-12-12 20:24:35 -0500

    E

--- src/1446_Consecutive_Characters.cpp
+++ src/1446_Consecutive_Characters.cpp
@@ -1,16 +1,22 @@
+/**
+ *  Time:
+ *  O(n)
+ *  Space:
+ *  O(1)
+ */
 class Solution {
 public:
     int maxPower(string s) {
         int result = 0;
-        int len = 1;
-        for (int i = 1; i < s.size(); ++i) {
-            if (s[i] == s[i-1]) {
-                ++len;
-            } else {
-                result = max(result, len);
-                len = 1;
+        for (int i = 0; i < s.size();) {
+            int l = 1;
+            while (i+1 < s.size() && s[i] == s[i+1]) {
+                ++l;
+                ++i;
             }
+            result = max(result, l);
+            ++i;
         }
-        return max(result, len);
+        return result;
     }
 };
