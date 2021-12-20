8a1c50e67d0a7bb888deaca6f06ef4f755f1f7d6
Liuyi Wang <wangliuy@umich.edu>
2021-12-19 19:33:03 -0500

    E

--- src/1200_Minimum_Absolute_Difference.cpp
+++ src/1200_Minimum_Absolute_Difference.cpp
@@ -1,31 +1,13 @@
-static int __ = []() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-	return 0;
-}();
-
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         sort(arr.begin(), arr.end());
-        vector<vector<int>> result;
+        map<int, vector<vector<int>>> hash;
         int diff = INT_MAX;
         for (int i = 1; i < arr.size(); ++i) {
-            if (arr[i]-arr[i-1] < diff) {
-                result.clear();
-                vector<int> v;
-                v.push_back(arr[i-1]);
-                v.push_back(arr[i]);
-                result.push_back(v);
-                diff = arr[i]-arr[i-1];
-            } else if (arr[i]-arr[i-1] == diff) {
-                vector<int> v;
-                v.push_back(arr[i-1]);
-                v.push_back(arr[i]);
-                result.push_back(v);
-            }
+            diff = min(diff, arr[i]-arr[i-1]);
+            hash[arr[i]-arr[i-1]].push_back({arr[i-1], arr[i]});
         }
-        return result;
+        return hash[diff];
     }
 };
