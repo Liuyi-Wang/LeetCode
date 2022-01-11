fd035c2591b7c2aa5274fa7013a39a4f5240711c
Liuyi Wang <wangliuy@umich.edu>
2022-01-10 21:47:56 -0500

    H

--- src/1223_Dice_Roll_Simulation.cpp
+++ src/1223_Dice_Roll_Simulation.cpp
@@ -9,35 +9,41 @@ public:
     int MODULE = 1e9+7;
     
     int dieSimulator(int n, vector<int>& rollMax) {
-        vector<vector<unsigned long>> dp(6, vector<unsigned long>(15, 0));
-        dp[0][0] = 1;
-        dp[1][0] = 1;
-        dp[2][0] = 1;
-        dp[3][0] = 1;
-        dp[4][0] = 1;
-        dp[5][0] = 1;
-        
+        vector<vector<vector<unsigned long>>> dp(2, vector<vector<unsigned long>>(6, vector<unsigned long>(15, 0)));
+        dp[0][0][0] = 1;
+        dp[0][1][0] = 1;
+        dp[0][2][0] = 1;
+        dp[0][3][0] = 1;
+        dp[0][4][0] = 1;
+        dp[0][5][0] = 1;
         for (int i = 1; i < n; ++i) {
-            vector<vector<unsigned long>> dpi(6, vector<unsigned long>(15, 0));
+            int d = i%2;
+            int dd = (i-1)%2;
             for (int j = 0; j < 6; ++j) {
-                for (int jj = 0; jj < 6; ++jj) {
-                    if (jj == j) {
-                        continue;
+                dp[d][j][0] = 0;
+                for (int k = 0; k < 6; ++k) {
+                    if (j == k) {
+                        for (int l = 1; l < rollMax[k]; ++l) {
+                            if (l == rollMax[j]) {
+                                break;
+                            }
+                            dp[d][j][l] = dp[dd][k][l-1];
+                        }
+                    } else {
+                        for (int l = 0; l < rollMax[k]; ++l) {
+                            dp[d][j][0] += dp[dd][k][l];
+                            dp[d][j][0] %= MODULE;
+                        }
                     }
-                    for (int k = 0; k < rollMax[jj]; ++k) {
-                        dpi[j][0] = (dpi[j][0]+dp[jj][k])%MODULE;
-                    }
-                }
-                for (int k = 1; k < rollMax[j]; ++k) {
-                    dpi[j][k] = dp[j][k-1];
                 }
             }
-            dp = dpi;
         }
-        unsigned long result = 0;
+        int result = 0;
+        int d = (n-1)%2;
         for (int j = 0; j < 6; ++j) {
-            for (int k = 0; k < rollMax[j]; ++k) {
-                result = (result+dp[j][k])%MODULE;
+            for (int l = 0; l < rollMax[j]; ++l) {
+                result += dp[d][j][l];
+                result %= MODULE;
             }
         }
         return result;
