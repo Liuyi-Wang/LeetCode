a27e533eb1d4a9840678e3c4cbfd69e1f6f25c2a
Liuyi Wang <wangliuy@umich.edu>
2022-01-10 21:21:57 -0500

    E

--- src/1022_Sum_of_Root_To_Leaf_Binary_Numbers.cpp
+++ src/1022_Sum_of_Root_To_Leaf_Binary_Numbers.cpp
@@ -1,36 +1,40 @@
 /**
+ *  Time:
+ *  O(n)
+ *  Space:
+ *  O(1)
+ */
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+    int result;
 public:
-    void solve(TreeNode* root, int sum) {
-        if (NULL == root->left && NULL == root->right) {
-            d_result += root->val+sum*2;
+    int sumRootToLeaf(TreeNode* root) {
+        result = 0;
+        dfs(root, 0);
+        return result;
+    }
+    
+    void dfs(TreeNode* root, int sum) {
+        sum = sum*2+root->val;
+        if (root->left == NULL && root->right == NULL) {
+            result += sum;
             return;
         }
-        if (NULL != root->left) {
-            solve(root->left, root->val+sum*2);
-        }        
-        if (NULL != root->right) {
-            solve(root->right, root->val+sum*2);
-        }        
-    }
-    
-    int sumRootToLeaf(TreeNode* root) {
-        if (NULL == root) {
-            return 0;
+        if (root->left) {
+            dfs(root->left, sum);
         }
-        d_result = 0;
-        solve(root, 0);
-        return d_result;
+        if (root->right) {
+            dfs(root->right, sum);
+        }
     }
-    
-private:
-    int d_result;
 };
