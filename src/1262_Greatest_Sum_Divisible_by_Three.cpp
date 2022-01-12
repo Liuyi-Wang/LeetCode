651e200a4b78e2ba535c215e82a1c82934587a51
Liuyi Wang <wangliuy@umich.edu>
2022-01-11 19:07:42 -0500

    M

--- src/1262_Greatest_Sum_Divisible_by_Three.cpp
+++ src/1262_Greatest_Sum_Divisible_by_Three.cpp
@@ -7,38 +7,54 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        vector<vector<int>> dp(2, vector<int>(3, 0));
-        int n = nums.size();
-        dp[0][nums[0]%3] = nums[0];
-        int index = 0;
-        for (int i = 1; i < n; ++i) {
-            index = i%2;
-            dp[index][0] = dp[(index+1)%2][0];
-            dp[index][1] = dp[(index+1)%2][1];
-            dp[index][2] = dp[(index+1)%2][2];
-            
-            if (nums[i]%3 == 0) {
-                dp[index][0] = max(dp[index][0], nums[i]);
-                
-                dp[index][0] = max(dp[index][0], (0==dp[(index+1)%2][0])?0:(dp[(index+1)%2][0]+nums[i]));
-                dp[index][1] = max(dp[index][1], (0==dp[(index+1)%2][1])?0:(dp[(index+1)%2][1]+nums[i]));
-                dp[index][2] = max(dp[index][2], (0==dp[(index+1)%2][2])?0:(dp[(index+1)%2][2]+nums[i]));
-            } else if (nums[i]%3 == 1) {
-                dp[index][1] = max(dp[index][1], nums[i]);
-                
-                dp[index][0] = max(dp[index][0], (0==dp[(index+1)%2][2])?0:(dp[(index+1)%2][2]+nums[i]));
-                dp[index][1] = max(dp[index][1], (0==dp[(index+1)%2][0])?0:(dp[(index+1)%2][0]+nums[i]));
-                dp[index][2] = max(dp[index][2], (0==dp[(index+1)%2][1])?0:(dp[(index+1)%2][1]+nums[i]));
-            } else {
-                dp[index][2] = max(dp[index][2], nums[i]);
-                
-                dp[index][0] = max(dp[index][0], (0==dp[(index+1)%2][1])?0:(dp[(index+1)%2][1]+nums[i]));
-                dp[index][1] = max(dp[index][1], (0==dp[(index+1)%2][2])?0:(dp[(index+1)%2][2]+nums[i]));
-                dp[index][2] = max(dp[index][2], (0==dp[(index+1)%2][0])?0:(dp[(index+1)%2][0]+nums[i]));
-            }
-            
-            //cout << dp[index][0] << "," << dp[index][1] << "," << dp[index][2] << endl;
+        int dp0 = 0, dp1 = 0, dp2 = 0;
+        if (nums[0]%3 == 0) {
+            dp0 = nums[0];
+        } else if (nums[0]%3 == 1) {
+            dp1 = nums[0];
+        } else {
+            dp2 = nums[0];
         }
-        return dp[index][0];
+        for (int i = 1; i < nums.size(); ++i) {
+            int dpi0 = dp0, dpi1 = dp1, dpi2 = dp2;
+            if (nums[i]%3 == 0) {
+                dpi0 = max(dpi0, nums[i]);
+                if (dp0 != 0) {
+                    dpi0 = dp0 + nums[i];
+                }
+                if (dp1 != 0) {
+                    dpi1 = dp1 + nums[i];
+                }
+                if (dp2 != 0) {
+                    dpi2 = dp2 + nums[i];
+                }
+            } else if (nums[i]%3 == 1) {
+                dpi1 = max(dpi1, nums[i]);
+                if (dp2 != 0) {
+                    dpi0 = max(dpi0, dp2 + nums[i]);
+                }
+                if (dp0 != 0) {
+                    dpi1 = max(dpi1, dp0 + nums[i]);
+                }
+                if (dp1 != 0) {
+                    dpi2 = max(dpi2, dp1 + nums[i]);
+                }
+            } else {
+                dpi2 = max(dpi2, nums[i]);
+                if (dp1 != 0) {
+                    dpi0 = max(dpi0, dp1 + nums[i]);
+                }
+                if (dp2 != 0) {
+                    dpi1 = max(dpi1, dp2 + nums[i]);
+                }
+                if (dp0 != 0) {
+                    dpi2 = max(dpi2, dp0 + nums[i]);
+                }
+            }
+            dp0 = dpi0;
+            dp1 = dpi1;
+            dp2 = dpi2;
+        }
+        return dp0;
     }
 };
